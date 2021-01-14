68b632b2cca538a74ff40a5c094bd849799c32c0
mhope <mhope@npontu.com>
2021-01-14 07:57:33 +0000

    commit

--- beta_survey_django/settings.py
+++ beta_survey_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g=33!vu8@_2t6hz#^us^-izxpd3rv760vvo31y5j@nle6kxx@g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['beta-survey.npontu.com', '127.0.0.1']
 
